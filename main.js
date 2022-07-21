94e3727d76a04f8a6ea5a01ba3126435a9cf4a33
greybeetle213 <65577606+greybeetle213@users.noreply.github.com>
2022-07-21 20:26:28 +1200

    began reposotory for the last time

---
+++ main.js
@@ -0,0 +1,14 @@
+SelectedBtn = "Home"
+function MenuButtonPress(btn){
+    if(SelectedBtn != btn){
+        document.getElementById(SelectedBtn).className = "menuBtn"
+        SelectedBtn = btn
+        document.getElementById(SelectedBtn).className = "selectedBtn"
+    }
+    document.getElementById("content").src = "pages/"+btn+".html"
+}
+function init(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        window.location.replace("mobile/index.html")
+    }
+}
---
+++ mobile/main.js
@@ -0,0 +1,42 @@
+SelectedBtn = "Home"
+MenuOpen = false
+function MenuButtonPress(btn){
+    if(SelectedBtn != btn){
+        document.getElementById(SelectedBtn).className = "menuBtn"
+        SelectedBtn = btn
+        document.getElementById(SelectedBtn).className = "selectedBtn"
+        document.getElementById("content").src = "pages/"+btn+".html"
+        MenuOpen = false
+        closeLoop = setInterval(CloseMenuAnimation,1)
+    }
+}
+function OpenMenu(){
+    openLoop = setInterval(OpenMenuAnimation,1)
+}
+
+function OpenMenuAnimation(){
+    document.getElementById("menu").style.left = String(Number(document.getElementById("menu").style.left.replace(/[^\d.-]/g, ''))+1)+"%"
+    if(document.getElementById("menu").style.left == "0%"){
+        MenuOpen = true
+        clearInterval(openLoop)
+    }
+}
+
+function CloseMenuAnimation(){
+    document.getElementById("menu").style.left = String(Number(document.getElementById("menu").style.left.replace(/[^\d.-]/g, ''))-1)+"%"
+    if(document.getElementById("menu").style.left == "-90%"){
+        clearInterval(closeLoop)
+    }
+}
+
+function init(){
+    var menu = document.getElementById('visibleMenu');
+
+    document.addEventListener('click', function(event) {
+        var isClickInsideElement = menu.contains(event.target);
+        if (!isClickInsideElement && MenuOpen) {
+            MenuOpen = false
+            closeLoop = setInterval(CloseMenuAnimation,1)
+        }
+    });
+}
